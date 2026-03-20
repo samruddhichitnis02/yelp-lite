bfc2d21b0bbcc39149c5064cc1e2aae616ce9df8
Samruddhi Chitnis <samruddhichitnis02@gmail.com>
2026-03-20 10:18:52 -0700

    added RestaurantDetailPublic schema with review count and reviews list

--- backend/schemas/restaurant.py
+++ backend/schemas/restaurant.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
 from schemas.review import ReviewPublic
+from pydantic import BaseModel, Field
 from typing import Optional, List
 
 class RestaurantCreateRequest(BaseModel):
